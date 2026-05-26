acdcf7b8a322e9bd32cfdf8b5442cd638836911e
irca8793-lang <irca8793@gmail.com>
2026-05-26 10:22:29 -0600

    Update variables.py

--- 02_variables/variables.py
+++ 02_variables/variables.py
@@ -1,1 +1,14 @@
+print("*******************************")
+print("**    BIENVENIDO A        **")
+print("** LA TIENDA DE MASCOTAS  **")
+print("*******************************")
 
+num_perros = 10
+num_gatos = 8
+num_pajaros = 25
+
+animales_totales = num_perros + num_gatos + num_pajaros
+
+print("Actualmente contamos con:")
+print("Perros:", num_perros, "Gatos:", num_gatos, "Pajaros:", num_pajaros)
+print("En total tenemos", animales_totales, "animales")
